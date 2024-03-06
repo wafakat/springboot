74a30c40a2f498277913b2a713b86f0bfc967a61
wafakat <wafakatar8@gmail.com>
2024-03-06 10:54:24 +0100

    first project springboot

---
+++ src/main/java/com/example/katar/KatarApplication.java
@@ -0,0 +1,13 @@
+package com.example.katar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/katar/KatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
